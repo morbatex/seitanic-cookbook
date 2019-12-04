af89b99afcbc5f3379b57b0312b8a2e005578428
Max Böcker <max.boecker@rwth-aachen.de>
2019-12-04 18:20:19 +0100

    removed not needed feature flag

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(proc_macro_hygiene, decl_macro, option_flattening)]
+#![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate mongodb;
